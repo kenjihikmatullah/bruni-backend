26051d41199112862e527b5213198c1082a53952
Kenji Hikmatullah <iniemailkenji@gmail.com>
2021-07-18 11:25:12 +0700

    shorten url

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const connection = require('./db.config')
 
 const app = express()
+
+// Connect to database
+connection.once('open', () => console.log('Connected to database'))
+connection.on('error', e => console.log(`Failed to connect to database: ${e.message}`))
+
+// Routes
+app.use(express.json({
+  extended: false
+}))
+app.use('/', require('./routes/redirect'))
+app.use('/api/url', require('./routes/url'))
 
 const PORT = process.env.PORT || 5000
 
